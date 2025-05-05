b3c9d1e8bbcca317ae06f3ee5d6095a8fd950fbf
l6a6h6l <64456111+l6a6h6l@users.noreply.github.com>
2025-05-05 03:50:54 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import DriverManagement from './components/DriverManagement';
 
+function App() {
+  return (
+    <div className="App">
+      <DriverManagement />
+    </div>
+  );
+}
+
+export default App;
